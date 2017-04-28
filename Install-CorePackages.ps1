22f2e260b92c0a0b29a91018e8fcbfb509163e07
Jed Green <wickedvikingstudios@gmail.com>
2017-04-27 23:22:07 -0500

    Install-CorePackages - remote install test

--- Install-CorePackages.ps1
+++ Install-CorePackages.ps1
@@ -2,7 +2,6 @@
 #
 #   Install-HOCorePackages.ps1
 #
-################################################################################
 #
 # Installs the following:
 #
@@ -16,8 +15,21 @@
 # Docker-For-Windows
 #
 ################################################################################
-
+$ExecutionPolicy = Get-ExecutionPolicy -Scope CurrentUser
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+
+################################################################################
+#
+# Update-Path
+#
+################################################################################
+Function Update-Path {
+    Process {
+        Write-Host "Refeshing path..."
+        $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") `
+            + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+    }
+}
 
 ################################################################################
 #
@@ -25,39 +37,30 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 #
 # Installs packages that have remote powershell script installers.
 #
-# -AppPath[string]
-#       Path where the program executable is typically installed
 # -AppName[string]
 #       Name of the program, doesn't have to match anything
+# -ExecutableName[string]
+#       The actual filename of the program, ex. choco.exe
 # -InstallUrl[string]
 #       Url of the the remost install script.
 # -UpdateCommand[string]
 #       Command to update the program
-# -TestPaths[string]
-#       Paths to test for existing installations (typically a path to standard
-#       executable locations)
 #
 ################################################################################
 function Install-HOPackageFromRemoteScript {
     [CmdletBinding()]
     Param(
         [String]$AppName,
-        [string]$AppPath,
+        [string]$ExecutableName,
         [String]$InstallUrl,
-        [String]$UpdateCommand,
-        [String[]]$TestPaths
+        [String]$UpdateCommand
     )
     Process {
         $OriginalErrorActionPreference = $ErrorActionPreference
+        $executablePath = (Get-Command -Name $executableName -ErrorAction SilentlyContinue).Path
 
-        $pathFound = $false
-
-        foreach ($path in $TestPaths) {
-            if( (Test-Path -Path "$AppPath$path") ) { $pathFound =$true }
-        }
-
-        if($pathFound) {
-            Write-Host "$AppName installation detected, updating to latest version"
+        if( $executablePath ) {
+            Write-Host "$AppName installation at $executablePath, updating to latest version"
             iex "& $UpdateCommand"
             $ErrorActionPreference = $OriginalErrorActionPreference
             if($LASTEXITCODE) {
@@ -73,6 +76,7 @@ function Install-HOPackageFromRemoteScript {
                 Exit $LASTEXITCODE
             }
         }
+        Update-Path
     }
 }
 
@@ -82,44 +86,27 @@ function Install-HOPackageFromRemoteScript {
 #
 # Installs packages from chocolatey
 #
-# -AppPath[string]
-#       Path where the program executable is typically installed by choco
 # -AppName[string]
-#       Name of the program
-# -TestPaths[string]
-#       Paths to test for existing installations (typically a path to standard
-#       executable locations)
+#       Name of the chocolatey package
+# -ExecutableName[string]
+#       The actual filename of the program, ex. choco.exe
+# -InstallParams[string]
+#       Optional: Any supported install params
 #
 ################################################################################
 function Install-HOPackageFromChocolatey {
     [CmdletBinding()]
     Param(
         [String]$AppName,
-        [string[]]$AppPaths = $AppPaths.split(" "),
-        [string]$InstallParams,
-        [String[]]$TestPaths
+        [string]$ExecutableName,
+        [string]$InstallParams = ""
     )
     Process {
         $OriginalErrorActionPreference = $ErrorActionPreference
+        $executablePath = (Get-Command -Name $executableName -ErrorAction SilentlyContinue).Path
 
-        $pathFound = $false
-
-        Write-Host "AppName: $AppName"
-        Write-Host "AppPaths: $AppPaths"
-        Write-Host "InstallParams: $InstallParams"
-        Write-Host "TestPaths: $TestPaths"
-
-
-        while (-Not $pathFound) {
-            foreach ($path in $TestPaths) {
-                foreach( $appPath in $AppPaths) {
-                    Write-Host "$appPath$path"
-                    if( (Test-Path -Path "$AppPath$path") ) { $pathFound =$true }
-                }
-            }
-        }
-        if($pathFound) {
-            Write-Host "$AppName installation detected, updating to latest version"
+        if($executablePath) {
+            Write-Host "$AppName installation at $executablePath, updating to latest version"
             iex "& choco upgrade -y $AppName"
             $ErrorActionPreference = $OriginalErrorActionPreference
             if($LASTEXITCODE) {
@@ -139,36 +126,75 @@ function Install-HOPackageFromChocolatey {
                 Exit $LASTEXITCODE
             }
         }
+        Update-Path
     }
 }
 
-#Scoop
-Install-HOPackageFromChocolatey -AppName "docker-for-windows" `
-    -AppPath ($env:path.split(";") | % { if($_.ToLower().Contains("docker")){return $_} }) `
-    -InstallParams "/GitAndUnixToolsOnPath" `
-    -TestPaths @("\bin\docker.exe","\docker.exe")
-Exit
+################################################################################
+#
+# Install-HOPackageFromHOPackageFromScoop
+#
+# Installs packages from chocolatey
+#
+# -AppName[string]
+#       Name of the scoop package
+# -ExecutableName[string]
+#       The actual filename of the program, ex. scoop.exe
+#
+################################################################################
+function Install-HOPackageFromScoop {
+    [CmdletBinding()]
+    Param(
+        [String]$AppName,
+        [string]$ExecutableName
+    )
+    Process {
+        $OriginalErrorActionPreference = $ErrorActionPreference
+        $executablePath = (Get-Command -Name $executableName -ErrorAction SilentlyContinue).Path
+
+        if($executablePath) {
+            Write-Host "$AppName installation at $executablePath, updating to latest version"
+            iex "&  scoop update $AppName"
+            $ErrorActionPreference = $OriginalErrorActionPreference
+            if($LASTEXITCODE) {
+                Write-Error "Update for $AppName was unable to execute comman via iex";
+                Exit $LASTEXITCODE
+            }
+        } else {
+            Write-Host "Installing $AppName..."
+            $expression = "& scoop install $AppName "
+            if ($InstallParams) {
+                $expression = $expression +" -params '$InstallParams'"
+            }
+            iex $expression
+            $ErrorActionPreference = $OriginalErrorActionPreference
+            if($LASTEXITCODE) {
+                Write-Error "Install for $AppName was unable to execute comman via iex";
+                Exit $LASTEXITCODE
+            }
+        }
+        Update-Path
+    }
+}
+
+#Test Exit
 ################################################################################
 # Begin Main Script
 ################################################################################
 
 #Scoop
 Install-HOPackageFromRemoteScript -AppName "Scoop" `
-    -AppPath ($env:SCOOP,"$env:USERPROFILE\scoop" | select -First 1) `
     -InstallUrl "https://get.scoop.sh" `
     -UpdateCommand "scoop update" `
-    -TestPaths @("\shims\scoop.ps1","\shims\scoop.cmd")
+    -ExecutableName "scoop.cmd"
 
 #Chocolatey
 Install-HOPackageFromRemoteScript -AppName "Chocolatey" `
-    -AppPath ($env:ChocolateyInstall,"$env:ALLUSERSPROFILE\chocolatey" | select -First 1) `
     -InstallUrl "https://chocolatey.org/install.ps1" `
     -UpdateCommand "choco upgrade chocolatey" `
-    -TestPaths @("\bin\choco.exe","\chocolatey\bin\choco.exe")
+    -ExecutableName "choco.exe"
 
-Write-Host "Refeshing path..."
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" `
-+ [System.Environment]::GetEnvironmentVariable("Path","User")
+
 
 #Hyper-V
 $hyperv = Get-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online
@@ -179,11 +205,16 @@ if($hyperv.state -eq "Enabled") {
     Enable-WindowsOptionalFeature -FeatureName Microsoft-Hyper-V-All -Online -NoRestart
 }
 
-#Git w/ Unix Tool on Command Line
-choco install -y git -params "/GitAndUnixToolsOnPath"
-scoop install packer vagrant
-choco install -y fciv
-choco install -y docker-for-windows
+Install-HOPackageFromChocolatey -AppName "docker-for-windows" -ExecutableName "docker.exe"
+Install-HOPackageFromChocolatey -AppName "git" -ExecutableName "git.exe" -InstallParams "/GitAndUnixToolsOnPath"
+
+Install-HOPackageFromChocolatey -AppName "fciv" -ExecutableName "fciv.exe"
+Install-HOPackageFromScoop -AppName "vagrant" -ExecutableName "vagrant.exe"
+Install-HOPackageFromScoop -AppName "packer" -ExecutableName "packer.exe"
+
+if (-Not (Test-Path "~/.ssh/id_rsa") ) { ssh-keygen -t rsa -f "~/.ssh/id_rsa" -P """" }
+
+Set-ExecutionPolicy -ExecutionPolicy $ExecutionPolicy -Scope CurrentUser -Force
 
 #Restart Computer to Finalize Hyper-V
 #Restart-Computer
--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 Hyper-OPs
-===
-A collection of solutions, utilities, recipes, and documentation for Open(ish) Source solutions on Windows for Windows and Hyper-V.
+=========
+My DevOps toolkit for dealing with Hyper-V.
+
+Install Options
+---------------
+**Remote PowerShell Option A** Install-CorePackages.ps1
+
+Installs the following package managers
+* [scoop](http://scoop.sh/) - Seems to have most current stable more
+often than chocolatey. Also install under user scope so no admin needed.
+Trying it out.
+* [chocolatey](https://chocolatey.org/) - Better install parameter
+support, wider selection, popping up in more and more places.
 
 [TODO](docs/TODO.md)
 
